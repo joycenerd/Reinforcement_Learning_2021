1ef49f51332a35c4849f5536fefabf3f40aab9c5
joycenerd <joycenerd.cs09@nycu.edu.tw>
2021-06-20 21:44:52 +0800

    v2

--- MPO/options.py
+++ MPO/options.py
@@ -1,86 +1,57 @@
 import argparse
 
-parser = argparse.ArgumentParser()
-parser.add_argument(
-    "--env",
-    type=str,
-    default="Hopper-v2",
-    help=
-    "(str) which task in the OpenAI gym to do: [BipedalWalker-v2, Hopper-v2, Acrobot-v1, CartPole-v0,Levitation-v0]"
-)
-parser.add_argument(
-    "--eps",
-    type=float,
-    default=0.1,
-    help="(float) hard constraint of the dual formulation in the E-step")
-parser.add_argument("--eps-mu",
-                    type=float,
-                    default=0.1,
-                    help="(float) hard constraint for mean in the M-step")
-parser.add_argument(
-    "--eps-sigma",
-    type=float,
-    default=1e-4,
-    help="(float) hard constraint for covarianve in the M-step")
-parser.add_argument("--lr", type=float, default=5e-4, help="learning rate")
-parser.add_argument(
-    "--alpha",
-    type=float,
-    default=10,
-    help="(float) scaling factor of the lagrangian multiplier in the M-step")
-parser.add_argument("--epochs",
-                    type=int,
-                    default=1000,
-                    help="(int) number of training epochs")
-parser.add_argument("--steps",
-                    type=int,
-                    default=1000,
-                    help="(int) number of steps in each epoch")
-parser.add_argument("--lagrange-iter",
-                    type=int,
-                    default=5,
-                    help="number of optimization steps of the Lagrangian")
-parser.add_argument("--batch-size",
-                    type=int,
-                    default=64,
-                    help="(int) size of the sampled mini-batch size")
-parser.add_argument("--rerun-mb",
-                    type=int,
-                    default=5,
-                    help="(int) number of reruns of the mini batch")
-parser.add_argument("--sample-epochs",
-                    type=int,
-                    default=1,
-                    help="(int) number of sampling episodes")
-parser.add_argument("--add_act",
-                    type=int,
-                    default=64,
-                    help="(int) number of additional actions")
-parser.add_argument("--policy_layers",
-                    type=int,
-                    default=(100, 100),
-                    help="(int,int) number of hidden layers in the policy net")
-parser.add_argument(
-    "--Q-layers",
-    type=int,
-    default=(200, 200),
-    help="(int,int) number of hidden layers in the Q function net")
-parser.add_argument("--log",
-                    type=bool,
-                    default=True,
-                    help="(bool) saves log if True")
-parser.add_argument("--log-dir",
-                    type=str,
-                    default="Hopper-v2",
-                    help="(str) log save dir name")
-parser.add_argument("--render",
-                    type=bool,
-                    default=False,
-                    help="(bool) renders the simulation if True")
-parser.add_argument("--checkpoint-dir",
-                    type=str,
-                    default="./checkpoints/",
-                    help="folder to save checkpoints")
-parser.add_argument("--domain",type=str,default="cartpole",help="control suite environment domain name")
-parser.add_argument("--task",type=str,default="balance",help="control suite environment task name")
+
+parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+        description='Implementation of MPO on gym environments')
+parser.add_argument('--device', type=str, default='cpu')
+parser.add_argument('--env', type=str, default='LunarLanderContinuous-v2',
+                    help='gym environment')
+parser.add_argument('--dual_constraint', type=float, default=0.1,
+                    help='hard constraint of the dual formulation in the E-step')
+parser.add_argument('--kl_mean_constraint', type=float, default=0.01,
+                    help='hard constraint of the mean in the M-step')
+parser.add_argument('--kl_var_constraint', type=float, default=0.0001,
+                    help='hard constraint of the covariance in the M-step')
+parser.add_argument('--kl_constraint', type=float, default=0.01,
+                    help='hard constraint in the M-step')
+parser.add_argument('--discount_factor', type=float, default=0.99,
+                    help='discount factor used in Policy Evaluation')
+parser.add_argument('--alpha_mean_scale', type=float, default=1.0,
+                    help='scaling factor of the lagrangian multiplier in the M-step')
+parser.add_argument('--alpha_var_scale', type=float, default=100.0,
+                    help='scaling factor of the lagrangian multiplier in the M-step')
+parser.add_argument('--alpha_scale', type=float, default=10.0,
+                    help='scaling factor of the lagrangian multiplier in the M-step')
+parser.add_argument('--alpha_mean_max', type=float, default=0.1,
+                    help='maximum value of the lagrangian multiplier in the M-step')
+parser.add_argument('--alpha_var_max', type=float, default=10.0,
+                    help='maximum value of the lagrangian multiplier in the M-step')
+parser.add_argument('--alpha_max', type=float, default=1.0,
+                    help='maximum value of the lagrangian multiplier in the M-step')
+parser.add_argument('--sample_episode_num', type=int, default=50,
+                    help='number of episodes to learn')
+parser.add_argument('--sample_episode_maxstep', type=int, default=300,
+                    help='maximum sample steps of an episode')
+parser.add_argument('--sample_action_num', type=int, default=64,
+                    help='number of sampled actions')
+parser.add_argument('--batch_size', type=int, default=64)
+parser.add_argument('--iteration_num', type=int, default=1000,
+                    help='number of iteration to learn')
+parser.add_argument('--episode_rerun_num', type=int, default=3,
+                    help='number of reruns of sampled episode')
+parser.add_argument('--mstep_iteration_num', type=int, default=5,
+                    help='the number of iterations of the M-Step')
+parser.add_argument('--evaluate_period', type=int, default=10,
+                    help='periode of evaluation')
+parser.add_argument('--evaluate_episode_num', type=int, default=100,
+                    help='number of episodes to evaluate')
+parser.add_argument('--evaluate_episode_maxstep', type=int, default=300,
+                    help='maximum evaluate steps of an episode')
+parser.add_argument('--log_dir', type=str, default="LunarLanderContinuous-v2",
+                    help='log directory')
+parser.add_argument('--render', action='store_true')
+parser.add_argument('--load', type=str, default=None,
+                    help='load path')
+args = parser.parse_args()
 args = parser.parse_args()
--- MPO/train.py
+++ MPO/train.py
@@ -1,26 +1,48 @@
 from options import args
-from mpo import MPO
-import mujoco_py
 
-from dm_control import suite
-import quanser_robots
+from mpo import MPO
 import gym
 
 
-if __name__ == "__main__":
-    print('\n'.join(f'{k}={v}' for k, v in vars(args).items()))
-    input("Press enter to continue: ")
-
-    # load env
-    domain=args.domain
-    task=args.task
+def main():
     gym.logger.set_level(40)
     env = gym.make(args.env)
-    # env=dm_control2gym.make(domain_name=domain, task_name=task, seed=1)
-    # env=suite.load(domain_name=domain,task_name=task)
 
-    # select model
-    model = MPO(env)
+    model = MPO(
+        args.device,
+        env,
+        log_dir=args.log_dir,
+        dual_constraint=args.dual_constraint,
+        kl_mean_constraint=args.kl_mean_constraint,
+        kl_var_constraint=args.kl_var_constraint,
+        kl_constraint=args.kl_constraint,
+        discount_factor=args.discount_factor,
+        alpha_mean_scale=args.alpha_mean_scale,
+        alpha_var_scale=args.alpha_var_scale,
+        alpha_scale=args.alpha_scale,
+        alpha_mean_max=args.alpha_mean_max,
+        alpha_var_max=args.alpha_var_max,
+        alpha_max=args.alpha_max,
+        sample_episode_num=args.sample_episode_num,
+        sample_episode_maxstep=args.sample_episode_maxstep,
+        sample_action_num=args.sample_action_num,
+        batch_size=args.batch_size,
+        episode_rerun_num=args.episode_rerun_num,
+        mstep_iteration_num=args.mstep_iteration_num,
+        evaluate_period=args.evaluate_period,
+        evaluate_episode_num=args.evaluate_episode_num,
+        evaluate_episode_maxstep=args.evaluate_episode_maxstep)
 
-    # model train
-    model.train()
+    if args.load is not None:
+        model.load_model(args.load)
+
+    model.train(
+        iteration_num=args.iteration_num,
+        log_dir=args.log_dir,
+        render=args.render)
+
+    env.close()
+
+
+if __name__ == '__main__':
+    main()
